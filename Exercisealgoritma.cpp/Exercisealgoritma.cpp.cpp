2fae18504e9ec89e340cf28b086d65f86dc59827
ramizakamala <mizarakamala@gmail.com>
2023-04-06 10:51:58 +0700

    Add project files.

---
+++ Exercisealgoritma.cpp/Exercisealgoritma.cpp.cpp
@@ -0,0 +1,16 @@
+//1. karena didalam suatu algoritma terdapat variabel,prosedur penyelesain dsb.oleh karna itu pentingnya alogirtma dalam pemecahan suatu masalah.
+//2. static and dynamic
+//3. factor time and space
+//4. insert short, mungkin alogirtma ini dikenal paling mudah untuk pengimplementasikannya 
+//5. struct
+//6. 
+
+#include <iostream>
+using namespace std;
+
+int ramiza[26];
+int cmp_count = 0;
+int mov_count = 0;
+int n;
+
+
